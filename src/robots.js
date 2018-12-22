0dae59cfaf470494c687303035aea41dd46f4239
Chike Chris <cchike2017@gmail.com>
2018-12-22 09:41:33 -0500

    readme updated

---
+++ src/robots.js
@@ -0,0 +1,76 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sinas@gmail.bnm'
+	},
+	{
+		id: 2,
+		name: 'Aeanne Graham',
+		username: 'Bret',
+		email: 'Sina@gmail.bnm'
+	},
+	{
+		id: 3,
+		name: 'Z Graham',
+		username: 'Bret',
+		email: 'Snas@gmail.bnm'
+	},
+	{
+		id: 4,
+		name: 'Qea Graham',
+		username: 'Bret',
+		email: 'Dinas@gmail.bnm'
+	},
+	{
+		id: 5,
+		name: 'Leanne Grah',
+		username: 'Bret',
+		email: 'Sifffonsts@gmail.bnm'
+	},
+	{
+		id: 6,
+		name: 'Lenne Bham',
+		username: 'Bret',
+		email: 'Sis@gmail.bnm'
+	},
+	{
+		id: 7,
+		name: 'Snne Graham',
+		username: 'Bret',
+		email: 'Sinanns@gmail.bnm'
+	},
+	{
+		id: 8,
+		name: 'Heanne Graham',
+		username: 'Bret',
+		email: 'Ainyyas@gmail.bnm'
+	},
+	{
+		id: 9,
+		name: 'Aeanne Graham',
+		username: 'Bret',
+		email: 'Binas@gmail.bnm'
+	},
+	{
+		id:10,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Qtg@gmail.bnm'
+	},
+	{
+		id:11,
+		name: 'Geanne Graham',
+		username: 'Bret',
+		email: 'Sitreg@gmail.bnbvc'
+	},
+	{
+		id:12,
+		name: 'GHanne Graham',
+		username: 'Bret',
+		email: 'Sizzg@gmail.bccd'
+	}
+
+]
+
